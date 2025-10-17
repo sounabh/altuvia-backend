621378639216e4b315fe5d3cabfc3e13877f484d
sounabh <bagsounabh2003@gmail.com>
2025-10-17 15:00:24 +0530

    domainadd

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "https://altuvia.vercel.app",
+      "https://altuvia-beta.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
